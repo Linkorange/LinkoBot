d70a8684a70693ad5559b857128d26077da62092
Linkorange <judorange1992@yahoo.fr>
2018-05-30 22:35:42 +0200

    Modified the command list by a JSON and automated command management

--- commands/command_utils.py
+++ commands/command_utils.py
@@ -1,13 +1,26 @@
+import json
+from quotes.quotes import quote_command_handling
+
 # FIXME: As the file is imported in ../linkobot.py, I had to add the relative path from it and not from command_utils.py
-with open('commands/command_list.txt') as command_list_file:
-    command_list = command_list_file.read().splitlines()
-
-    def is_cmd_in_list(cmd, target):
-        """Checks if the given command is in the command_list"""
-        if target in command_list and cmd.split(' ')[0] == target:
-            return True
-        return False
 
 
-if __name__ == "__main__":
-    pass
+def write_in_chat_from_cmd(full_cmd):
+    """Returns a string given a command line according to the command_list.json JSON file"""
+    cmd = full_cmd.split(' ')[0]
+    with open('commands/command_list.json') as command_list_json:
+        command_list = json.load(command_list_json)
+        if cmd in command_list.keys():
+            if command_list[cmd] == "complex_treatment":
+                return complex_treatment(full_cmd)
+            return command_list[cmd]
+        return ''
+
+
+def complex_treatment(cmd):
+    """Defines more complex treatments than just returning a string given a command
+
+    Usually this function passes commands to functions which will parse them and make some treatments"""
+    core_command = cmd.split(' ')[0]
+    if core_command == "quote":
+        return quote_command_handling(cmd)
+    return ''
--- linkobot.py
+++ linkobot.py
@@ -1,8 +1,7 @@
 import sys
 import irc.bot
 import requests
-from quotes.quotes import quote_command_handling
-from commands.command_utils import is_cmd_in_list
+from commands.command_utils import write_in_chat_from_cmd
 
 
 class LinkoBot(irc.bot.SingleServerIRCBot):
@@ -14,7 +13,7 @@ class LinkoBot(irc.bot.SingleServerIRCBot):
         # Get the channel ID
         url = 'https://api.twitch.tv/kraken/users?login=' + channel
         headers = {'Client-ID': client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
-        r = requests.get(url, headers = headers).json()
+        r = requests.get(url, headers=headers).json()
         self.channel_id = r['users'][0]['_id']
 
         # Create IRC bot connection
@@ -39,35 +38,7 @@ class LinkoBot(irc.bot.SingleServerIRCBot):
         return
 
     def do_command(self, e, cmd):
-        c = self.connection
-
-        if is_cmd_in_list(cmd, 'bb'):
-            self.display_message_in_chat(c, 'A sheet explaining everything about blue balls : https://goo.gl/7MH1MG')
-
-        if is_cmd_in_list(cmd, 'quote'):
-            self.display_message_in_chat(c, quote_command_handling(cmd))
-
-        if is_cmd_in_list(cmd, 'discord'):
-            self.display_message_in_chat(c, 'Join my Discord server ! https://discord.gg/bqeugS')
-
-        if is_cmd_in_list(cmd, 'twitter'):
-            self.display_message_in_chat(c, 'Follow me on Twitter ! https://twitter.com/Linkorange')
-
-        if is_cmd_in_list(cmd, 'yt'):
-            self.display_message_in_chat(c, 'See all my videos on my Youtube channel:'
-                                         + 'https://www.youtube.com/user/Linkorange')
-
-        if is_cmd_in_list(cmd, 'youtube'):
-            self.display_message_in_chat(c, 'See all my videos on my Youtube channel:'
-                                         + 'https://www.youtube.com/user/Linkorange')
-
-        if is_cmd_in_list(cmd, 'sn'):
-            self.display_message_in_chat(c, 'Follow me on Twitter ! https://twitter.com/Linkorange '
-                                         + 'And don\'t forget to join my Discord server :) https://discord.gg/bqeugS'
-                                         + 'Also find all my videos on https://www.youtube.com/user/Linkorange')
-
-    def display_message_in_chat(self, c, message):
-        c.privmsg(self.channel, message)
+        self.connection.privmsg(self.channel, write_in_chat_from_cmd(cmd))
 
 
 def main():
