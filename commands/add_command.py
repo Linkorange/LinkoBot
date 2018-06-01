220e65292f7e3718458e4f6bff2951ff0774ed69
Linkorange <judorange1992@yahoo.fr>
2018-06-01 21:14:40 +0200

    Added an "add command" script

---
+++ commands/add_command.py
@@ -0,0 +1,25 @@
+import json
+import sys
+
+
+def add_command(cmd_name, cmd_msg='', help_msg=''):
+    with open('commands/command_list.json', 'r+') as command_list_json:
+        command_list = json.load(command_list_json)
+        command_list[cmd_name] = {'command': cmd_msg, 'help': help_msg}
+
+        command_list_json.seek(0)
+        command_list_json.truncate()
+        json.dump(command_list, command_list_json)
+        print('Successfully added the following command in command_list.json : '
+              + str({cmd_name: command_list[cmd_name]}))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        add_command(sys.argv[1])
+    elif len(sys.argv) == 3:
+        add_command(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:
+        add_command(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        print('Wrong number of arguments! Type "add_command.py <cmd_name> [<cmd_msg>] [<help_msg>]"')
